39155e1ab58953ea0169a320f38f8ec9757b509f
Arno Dekker <adekker@gmail.com>
2015-11-29 10:29:45 +0100

    added swedish language

---
+++ translations/harbour-acid-pass-sv.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="24"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="43"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="48"/>
+        <source>Shows stored Wi-Fi related passwords</source>
+        <translation>Visar lagrade WiFi-relaterade lösenord</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="57"/>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="58"/>
+        <source>Wrong code!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="84"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="88"/>
+        <source>Change access code</source>
+        <translation>Byt åtkomstkod</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="99"/>
+        <source>Hide all passwords</source>
+        <translation>Dölj alla lösenord</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="105"/>
+        <source>Show all passwords</source>
+        <translation>Visa alla lösenord</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="113"/>
+        <source>Acid-pass</source>
+        <translation>Acid-lösen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="124"/>
+        <source>No stored WiFi connections found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="179"/>
+        <source>Copy password to clipboard</source>
+        <translation>Kopiera lösenordet till urklipp</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordPage</name>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="36"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="37"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="45"/>
+        <source>Enter your 4 digit access code</source>
+        <translation>Ange din 4-siffriga åtkomstkod</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="45"/>
+        <source>Set a new 4 digit access code</source>
+        <translation>Ange ny 4-siffrig åtkomstkod</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="58"/>
+        <source>Enter access code</source>
+        <translation>Ange åtkomstkod</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="70"/>
+        <source>Your access code</source>
+        <translation>Din åtkomstkod</translation>
+    </message>
+</context>
+</TS>
