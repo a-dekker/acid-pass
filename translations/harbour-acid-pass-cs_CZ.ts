520ece038de8d806eb20260fa4500828b01f6e7b
Dominik Chrástecký <dominik@chrastecky.eu>
2015-12-03 22:07:59 +0100

    Added czech translation

---
+++ translations/harbour-acid-pass-cs_CZ.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>About</source>
+        <translation>O aplikaci</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="24"/>
+        <source>Info</source>
+        <translation>Informace</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="43"/>
+        <source>Version</source>
+        <translation>Verze</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="48"/>
+        <source>Shows stored Wi-Fi related passwords</source>
+        <translation>Zobrazuje uložená hesla k Wi-Fi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="57"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="58"/>
+        <source>Wrong code!</source>
+        <translation>Špatný kód!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="84"/>
+        <source>About</source>
+        <translation>O aplikaci</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="88"/>
+        <source>Change access code</source>
+        <translation>Změnit přístupový kód</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="99"/>
+        <source>Hide all passwords</source>
+        <translation>Skrýt všechna hesla</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="105"/>
+        <source>Show all passwords</source>
+        <translation>Zobrazit všechna hesla</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="113"/>
+        <source>Acid-pass</source>
+        <translation>Acid-pass</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="124"/>
+        <source>No stored WiFi connections found</source>
+        <translation>Nenalezena žádná uložená WiFi připojení</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="179"/>
+        <source>Copy password to clipboard</source>
+        <translation>Zkopírovat heslo do schránky</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordPage</name>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="36"/>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="37"/>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="45"/>
+        <source>Enter your 4 digit access code</source>
+        <translation>Vložte svůj čtyřmístný přístupový kód</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="45"/>
+        <source>Set a new 4 digit access code</source>
+        <translation>Nastavte nový čtyřmístný přístupový kód</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="58"/>
+        <source>Enter access code</source>
+        <translation>Vložte přístupový kód</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="70"/>
+        <source>Your access code</source>
+        <translation>Váš přístupový kód</translation>
+    </message>
+</context>
+</TS>
