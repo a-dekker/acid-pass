06e18194481a36411a18ec98a778993b78add70e
julius.jann <julius.jann@fyber.com>
2016-03-17 18:08:09 +0100

    add German Translations

---
+++ translations/harbour-acid-pass-de.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="24"/>
+        <source>Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="43"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="48"/>
+        <source>Shows stored Wi-Fi related passwords</source>
+        <translation>Zeige gespeicherte Wlan Passwörter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="57"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="58"/>
+        <source>Wrong code!</source>
+        <translation>Falscher Code!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="84"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="88"/>
+        <source>Change access code</source>
+        <translation>Zugangscode ändern</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="99"/>
+        <source>Hide all passwords</source>
+        <translation>Alle Passwörter verstecken</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="105"/>
+        <source>Show all passwords</source>
+        <translation>Alle Passwörter anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="113"/>
+        <source>Acid-pass</source>
+        <translation>Acid-pass</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="124"/>
+        <source>No stored WiFi connections found</source>
+        <translation>Keine gesicherten Wlan Verbindungen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="190"/>
+        <source>Copy password to clipboard</source>
+        <translation>Passwort in den Zwichenspeicher kopieren</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordPage</name>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="36"/>
+        <source>Save</source>
+        <translation>Sichern</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="37"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="45"/>
+        <source>Enter your 4 digit access code</source>
+        <translation>Gib Deinen vierstelligen Zugangscode ein</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="45"/>
+        <source>Set a new 4 digit access code</source>
+        <translation>Einen neuen vierstelligen Zugangscode erstellen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="59"/>
+        <source>Enter access code</source>
+        <translation>Zugangscode eingeben</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PasswordPage.qml" line="71"/>
+        <source>Your access code</source>
+        <translation>Dein Zugangscode</translation>
+    </message>
+</context>
+</TS>
