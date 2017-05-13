8b3c156c7a1e630b54136a9573bd5f5864253b3e
Carlos   Gonzalez <carlosgonz@protonmail.com>
2017-05-13 17:40:15 -0400

    Update harbour-acid-pass-es.ts

--- harbour-acid-pass-es.ts
+++ harbour-acid-pass-es.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../qml/pages/About.qml" line="23"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <location filename="../qml/pages/About.qml" line="26"/>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Info</translation>
     </message>
     <message>
         <location filename="../qml/pages/About.qml" line="45"/>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versión</translation>
     </message>
     <message>
         <location filename="../qml/pages/About.qml" line="50"/>
         <source>Shows stored Wi-Fi related passwords</source>
-        <translation type="unfinished"></translation>
+        <translation>Muestra las claves Wi-Fi almacenadas</translation>
     </message>
     <message>
         <location filename="../qml/pages/About.qml" line="59"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor</translation>
     </message>
 </context>
 <context>
@@ -34,42 +34,42 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="81"/>
         <source>Wrong code!</source>
-        <translation type="unfinished"></translation>
+        <translation>Código erroneo!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="107"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="111"/>
         <source>Change access code</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiar código de acceso</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="122"/>
         <source>Hide all passwords</source>
-        <translation type="unfinished"></translation>
+        <translation>Ocultar Claves</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="129"/>
         <source>Show all passwords</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar Claves</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="138"/>
         <source>Acid-pass</source>
-        <translation type="unfinished"></translation>
+        <translation>Acid-pass</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="149"/>
         <source>No stored WiFi connections found</source>
-        <translation type="unfinished"></translation>
+        <translation>Sin claves Wi-Fi guardadas</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="220"/>
         <source>Copy password to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Copiar clave al portapapeles</translation>
     </message>
 </context>
 <context>
@@ -77,37 +77,37 @@
     <message>
         <location filename="../qml/pages/PasswordPage.qml" line="49"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <location filename="../qml/pages/PasswordPage.qml" line="65"/>
         <source>Acid-pass</source>
-        <translation type="unfinished"></translation>
+        <translation>Acid-pass</translation>
     </message>
     <message>
         <location filename="../qml/pages/PasswordPage.qml" line="73"/>
         <source>Enter your 4 digit access code</source>
-        <translation type="unfinished"></translation>
+        <translation>Introducir los 4 digitos</translation>
     </message>
     <message>
         <location filename="../qml/pages/PasswordPage.qml" line="73"/>
         <source>Set a new 4 digit access code</source>
-        <translation type="unfinished"></translation>
+        <translation>Crear nuevo código de acceso</translation>
     </message>
     <message>
         <location filename="../qml/pages/PasswordPage.qml" line="87"/>
         <source>Enter access code</source>
-        <translation type="unfinished"></translation>
+        <translation>Introducir contraseña</translation>
     </message>
     <message>
         <location filename="../qml/pages/PasswordPage.qml" line="101"/>
         <source>Invalid access code!</source>
-        <translation type="unfinished"></translation>
+        <translation>Código de acceso invalido!</translation>
     </message>
     <message>
         <location filename="../qml/pages/PasswordPage.qml" line="104"/>
         <source>Your access code</source>
-        <translation type="unfinished"></translation>
+        <translation>Su código de acceso</translation>
     </message>
 </context>
 </TS>
