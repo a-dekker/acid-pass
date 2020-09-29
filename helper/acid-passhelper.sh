9ef27c75ba41c4e4aaeb7e91db68c593f8703b5a
arno <adekker@gmail.com>
2020-09-29 23:42:06 +0200

    fix for SFOS 3.4.0.22

--- helper/acid-passhelper.sh
+++ helper/acid-passhelper.sh
@@ -4,11 +4,13 @@ if [ ! -t 1 ]; then
     . /etc/sailfish-release
     MAIN_OS_VERSION=$(echo "${VERSION_ID}" | cut -c-3 | tr -d ".")
     if [ "${MAIN_OS_VERSION}" -lt 31 ]; then
-        CONNMAN_PATH=""
+        CONNMAN_PATH="/var/lib/connman"
+    elif [ "${MAIN_OS_VERSION}" -lt 34 ]; then
+        CONNMAN_PATH="/home/.system/var/lib/connman"
     else
-        CONNMAN_PATH="/home/.system"
+        CONNMAN_PATH="/home/nemo/.local/share/system/privileged/connman"
     fi
-    for FILE in "${CONNMAN_PATH}"/var/lib/connman/wifi*/settings; do
+    for FILE in "${CONNMAN_PATH}"/wifi*/settings; do
         NAME=$(grep "Name=" <"${FILE}" | sed "s/^Name=//g")
         if [ -z "${NAME}" ]; then
             break
